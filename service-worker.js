b554ee58a3a48b4101685e7483181afc41bb02c0
mathioupicchu <139147980+mathioupicchu@users.noreply.github.com>
2025-12-05 21:36:45 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "sufi-alcool-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/page2.html",
+  "/page3.html",
+  "/page4.html",
+  "/page5.html"
+];
+
+// Installation du service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activation et nettoyage du cache
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
+  );
+});
+
+// Interception des requêtes
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
